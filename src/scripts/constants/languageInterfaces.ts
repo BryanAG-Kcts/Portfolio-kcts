57a9ee03e052bfca2e5651ec99cba83b0688658d
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2023-12-25 19:56:57 -0500

    Error con isLoading

--- src/scripts/constants/languageInterfaces.ts
+++ src/scripts/constants/languageInterfaces.ts
@@ -7,7 +7,6 @@ export interface ILanguageContext extends ILanguage{
     setSelectedLanguage: (language: possibleLanguages) => void
     setLanguage: (language: ILanguage) => void
     selectedLanguage: possibleLanguages
-    isLoading: boolean
 }
 
 export interface ICachedLanguage {
