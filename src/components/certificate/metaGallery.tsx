620acefe1db08a16ceb97b40af7534e254d3a736
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2024-02-05 16:10:48 -0500

    meta

--- src/components/certificate/metaGallery.tsx
+++ src/components/certificate/metaGallery.tsx
@@ -1,12 +1,17 @@
+import { useContext } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { languageContext } from '../../scripts/constants/languageConstants'
 
 export const MetaGallery = () => {
+  const { selectedLanguage } = useContext(languageContext)
+
   return (
     <Helmet>
       <title>Kactuswow - 🎓🌟</title>
       <meta name='description' content='Una simple galeria sobre el camino de mi estudio' />
       <meta name='keywords' content='kactuswow, kactus, kcts, certificados, programador, ocaña, portafolio' />
       <meta name='robots' content='index, follow' />
+      <link rel='canonical' href={`https://portfolio-kcts.vercel.app/langs/${selectedLanguage}`} />
     </Helmet>
   )
 }
